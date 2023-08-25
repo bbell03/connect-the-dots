6654344bba2dd4ac694e80c12869c02cdcaf4bb9
bbell03 <46859217+bbell03@users.noreply.github.com>
2023-08-24 22:19:06 -0400

    added function mouse up

--- src/App.js
+++ src/App.js
@@ -16,8 +16,98 @@ var isDown; // if the mouse is down (dragging line)
 var lineExists = false; // At least one line exists
 var totalLines = 0; // total number of lines connecting dots
 var activeColor = "#FFF"; // color of active line
+var color_scheme = [
+    "FFFFFF"
+];
 var allLines = []; // array of all drawn lines
 // var gameOver = false;
+
+function addNewDots(coordsToGenerateDots, canvas) {
+    for (var i = 0; i < coordsToGenerateDots.length; i++) {
+
+        var random_color = Math.floor(Math.random() * (color_scheme.length - 0)) + 0;
+
+        var circ = new fabric.Circle({
+            id: coordsToGenerateDots[i].id,
+            in: false,
+            out: false,
+            left: coordsToGenerateDots[i].x,
+            top: coordsToGenerateDots[i].y - 20,
+            radius: 15,
+            fill: color_scheme[random_color],
+            opacity: 0,
+            strokeWidth: 5,
+            stroke: 'rgba(0,0,0,0)',
+            originX: 'left',
+            originY: 'top',
+            centeredRotation: true,
+            selectable: false
+        });
+        canvas.add(circ);
+
+        circ.animate({
+            'opacity': '1',
+            'top': circ.top + 20
+        }, {
+            duration: 950,
+            onChange: canvas.renderAll.bind(canvas),
+            onComplete: function () {
+
+            },
+            easing: fabric.util.ease["easeOutElastic"]
+        });
+    }
+
+}
+
+function removeSelectedDots(canvas) {
+    var coordsToGenerateDots = [];
+
+    // Remove selected dots
+    canvas.forEachObject(function (obj) {
+        if (linked_list._length <= 1) return;
+
+        if (obj.get('type') == "line") {
+            obj.animate('opacity', '0', {
+                duration: 75,
+                onChange: canvas.renderAll.bind(canvas),
+                onComplete: function () {
+                    canvas.remove(obj);
+                }
+            });
+            return;
+        }
+
+        var currentNode = linked_list.head;
+
+        for (var i = 0; i < linked_list._length; i++) {
+            if (currentNode.id == obj.id) {
+
+                coordsToGenerateDots.push({
+                    "x": obj.left,
+                    "y": obj.top,
+                    "id": obj.id
+                });
+
+                obj.animate('opacity', '0', {
+                    duration: 75,
+                    onChange: canvas.renderAll.bind(canvas),
+                    onComplete: function () {
+                        canvas.remove(obj);
+
+                    }
+                });
+            }
+
+            currentNode = currentNode.next;
+        }
+    });
+
+    lineExists = false;
+    linked_list = new DoublyList();
+    return coordsToGenerateDots;
+}
+
 
 function makeLine(x_start, x_end, y_start, y_end) {
   let line = new fabric.Line([x_start, x_end, y_start, y_end], {
@@ -26,6 +116,11 @@ function makeLine(x_start, x_end, y_start, y_end) {
 
   return line
 }
+
+//As of now I don't think this program uses react fabric
+
+//really try and isolate functionality in the vanilla version to
+//translate to react
 
 // function mouseDown() {
 //   if (o.target && o.target.get('type') == "circle") {
@@ -71,7 +166,7 @@ function makeLine(x_start, x_end, y_start, y_end) {
 //       width: 800,
 //       backgroundColor: 'pink'
 //     })
-//   )  
+//   )
 //   return(
 //     <div>
 //       <h1>Fabric.js on React - fabric.Canvas('...')</h1>
@@ -92,7 +187,7 @@ function App() {
   //   canvas.current.clear();
   //   //?
   //   // linked_list.clear();
-    
+
   //   linked_list = new DoublyList();
   //   allLines = [];
   //   totalLines = 0;
@@ -155,8 +250,8 @@ function App() {
               // Line exists as soon as user begins dragging
               // Snaps line to a circle that is hovered over, this can only occur when the user is dragging so isDown needs to be true
               if (line) {
-                  // e.target.getFill() == 
-                  // line.getStroke() && 
+                  // e.target.getFill() ==
+                  // line.getStroke() &&
                   if (isDown) {
 
                       lockLine = true;
@@ -278,9 +373,44 @@ function App() {
 
   });
 
-  // canvas.current.on('mouse:up', function(o) {
-  //   console.log('mouse up');
-  //   console.log(o);
+  // canvas.current.on('mouse:up', function (o) {
+  //     isDown = false;
+  //
+  //     // If mouse/line is let go on top of empty canvas and not a circle, delete the line
+  //     if (o.target === null) {
+  //         line.remove();
+  //     }
+  //
+  //     // otherwise, the mouse/line was released on a circle, and we much error check the line
+  //     else {
+  //         console.log(line);
+  //         // First if statement snaps/locks line with the same colored dot
+  //         // Diagonal lines are not allowed
+  //         if (line.x2 != line.x1 && line.y2 != line.y1) {
+  //             console.log("Diagonal lines are not allowed.");
+  //             line.remove();
+  //         }
+  //         // Lines can only be one unit long -- prevents line from streching across multiple dots
+  //         else if (Math.abs(line.x2 - line.x1) > grid || Math.abs(line.y2 - line.y1) > grid) {
+  //             console.log("Lines can only be one unit long");
+  //             lockLine = false;
+  //             line.remove();
+  //         }
+  //     }
+  //
+  //
+  //
+  //     // var activeDotsColumns = getColumnsToAddDotsTo(linked_list, CIRCLES_PER_ROW);
+  //     // console.log(activeDotsColumns);
+  //     // var dotsToAddPerColumn = shiftDotsDown(activeDotsColumns);
+  //     // console.log(dotsToAddPerColumn);
+  //     var coordsToGenerateDots = removeSelectedDots(canvas.current);
+  //     addNewDots(coordsToGenerateDots, canvas.current);
+  //     console.log(coordsToGenerateDots);
+  //
+  //
+  //
+  //
   // });
 
 }
